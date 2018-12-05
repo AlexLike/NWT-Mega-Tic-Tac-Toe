ecf5ef7ccb91cfce0a0be34799554e6808f56c10
Alexander Zank <alex.zank@icloud.com>
2018-12-05 09:02:24 +0100

    Declare LED pins, bug-fix mouse support, add dispenseAndReloadMagazine method

--- mainApp.ino
+++ mainApp.ino
@@ -6,9 +6,9 @@
 
 // MARK: - Dependencies
 
-#include <PS2Mouse.h>
 #include <Adafruit_MotorShield.h>
 #include <Servo.h>
+#include <PS2Mouse.h>
 
 // MARK: - Global definitions
 
@@ -18,6 +18,13 @@
 #define stepperRPM 20       // Desired # of rotations per minute
 #define mouseDataPin 1      // The mouse's PS/2 conforming data pin on Arduino (no PWM needed)
 #define mouseClockPin 2     // The mouse's PS/2 conforming clock pin on Arduino (no PWM needed)
+#define led1Pin 3           // The selection indicator LED pin for column 1
+#define led2Pin 4           // ... 2
+#define led3Pin 5           // ... 3
+#define led4Pin 6           // ... 4
+#define led5Pin 7           // ... 5
+#define led6Pin 8           // ... 6
+#define led7Pin 9           // ... 7
 
 
 // MARK: - Global properties
@@ -25,7 +32,7 @@
 Adafruit_MotorShield motorshield = Adafruit_MotorShield();                                          // The motorshield object, initialized at the beginning
 Adafruit_StepperMotor *positionStepper = motorshield.getStepper(stepperSteps, stepperDataPin);      // The stepper motor reference, retreived from the motorshield by defining the step # and data pin
 Servo magazineServo = Servo();   
-PS2Mouse mouse = PS2Mouse()                                                                   // The servo object, initialized at the beginning
+PS2Mouse mouse = PS2Mouse(mouseClockPin, mouseDataPin);                                                                   // The servo object, initialized at the beginning
 
 
 // MARK: - Main lifecycle
@@ -34,8 +41,9 @@ void setup() {
     // Establish Serial connection for debugging
 	Serial.begin(9600);
     Serial.println("Tic-Tac-Toe!");
-    // Configure magazineServo
+    // Configure magazineServo and set to center position
     magazineServo.attach(servoDataPin);
+    magazineServo.write(90);
     // Configure positionStepper speed
     positionStepper->setSpeed(stepperRPM);
     // Initialize mouse
@@ -46,7 +54,7 @@ void loop() {
     // DEBUG
     MouseData mouseData = mouse.readData();
     Serial.println(mouseData.position.x);
-    Serial.println(mouseData.position.y)
+    Serial.println(mouseData.position.y);
     delay(20);
 }
 
@@ -56,4 +64,15 @@ void serialEvent() {
     // DEBUG
     magazineServo.write(serialInput);
     positionStepper->step(serialInput, FORWARD, DOUBLE);
+}
+
+void dispenseAndReloadMagazine() {
+    // Dispense chip
+    magazineServo.write(180);
+    delay(1000);
+    // Reload magazine
+    magazineServo.write(60);
+    delay(1000);
+    // Reset to center position
+    magazineServo.write(90);
 }
